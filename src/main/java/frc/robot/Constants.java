f7d9028c4200a47c7934074d631da96a90d0d20a
Joshua Purushothaman <joshuathooyavan1@gmail.com>
2022-10-04 09:58:12 -0500

    TODO comments in Constants.java.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,17 +11,13 @@ public final class Constants {
   public static final double stickDeadband = 0.1;
 
   public static final class Swerve {
-    public static final int pigeonID = 6;
-    public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
+    public static final boolean invertGyro = true; // Always ensure Gyro is CCW+ CW-
 
     /* Drivetrain Constants */
     public static final double trackWidth = Units.inchesToMeters(21.5);
     public static final double wheelBase = Units.inchesToMeters(23.25);
     public static final double wheelDiameter = Units.inchesToMeters(4.0);
     public static final double wheelCircumference = wheelDiameter * Math.PI;
-
-    public static final double openLoopRamp = 0.25;
-    public static final double closedLoopRamp = 0.0;
 
     public static final double driveGearRatio = (6.75 / 1.0); // MK4 L2
     public static final double angleGearRatio = (12.8 / 1.0); // 12.8:1
@@ -38,7 +34,7 @@ public final class Constants {
     public static final int driveContinuousCurrentLimit = 35;
 
     /* Angle Motor PID Values */
-    public static final double angleKP = 0.99;
+    public static final double angleKP = 0.99;  //  Let's try leaving the PID and FF tuning alone for today. It might even work as-is.
     public static final double angleKI = 0.0;
     public static final double angleKD = 0.0;
 
@@ -70,45 +66,46 @@ public final class Constants {
     /* Module Specific Constants */
     /* Front Left Module - Module 0 */
     public static final class Mod0 {
-      public static final int driveMotorID = 8;
-      public static final int angleMotorID = 5;
-      public static final int canCoderID = 1;
-      public static final double angleOffset = 144.22;
+      public static final int driveMotorID = 8; //  TODO: verify ID that the drive and angle motors are correct; Left is drive and Right is angle
+      public static final int angleMotorID = 5; //  TODO: verify ID
+      public static final int canCoderID = 1; //  TODO: verify CANCoder ID (May have to wait for Josh)
+      public static final double angleOffset = 144.22;  //  TODO: change offset (Wait for Josh)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
 
     /* Front Right Module - Module 1 */
     public static final class Mod1 {
-      public static final int driveMotorID = 4;
-      public static final int angleMotorID = 2;
-      public static final int canCoderID = 2;
-      public static final double angleOffset = 109.07;
+      public static final int driveMotorID = 4; //  TODO: verify ID
+      public static final int angleMotorID = 2; //  TODO: verify ID
+      public static final int canCoderID = 2; //  TODO: verify CANCoder ID (May have to wait for Josh)
+      public static final double angleOffset = 109.07;  //  TODO: change offset (Wait for Josh)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
 
     /* Back Left Module - Module 2 */
     public static final class Mod2 {
-      public static final int driveMotorID = 10;
-      public static final int angleMotorID = 1;
-      public static final int canCoderID = 3;
-      public static final double angleOffset = 326.07;
+      public static final int driveMotorID = 10;  //  TODO: verify ID
+      public static final int angleMotorID = 1; //  TODO: verify ID
+      public static final int canCoderID = 3; //  TODO: verify CANCoder ID (May have to wait for Josh)
+      public static final double angleOffset = 326.07;  //  TODO: change offset (Wait for Josh)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
 
     /* Back Right Module - Module 3 */
     public static final class Mod3 {
-      public static final int driveMotorID = 7;
-      public static final int angleMotorID = 3;
-      public static final int canCoderID = 4;
-      public static final double angleOffset = 157.76;
+      public static final int driveMotorID = 7; //  TODO: verify ID
+      public static final int angleMotorID = 3; //  TODO: verify ID
+      public static final int canCoderID = 4; //  TODO: verify CANCoder ID (May have to wait for Josh)
+      public static final double angleOffset = 157.76;  //  TODO: change offset (Wait for Josh)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
   }
 
+  // Not going to use Auto unless we have more time. :(
   public static final class AutoConstants {
     public static final double kMaxSpeedMetersPerSecond = 3;
     public static final double kMaxAccelerationMetersPerSecondSquared = 3;
@@ -125,3 +122,15 @@ public final class Constants {
             kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
   }
 }
+
+//  TODO: If you complete everything in this file,
+//  Check electrical issues
+//  Set the CANCoder IDs (google how to do that; it's very similar to setting ID for a TalonSRX (can google that too))
+  //  It would be in the Phoenix Tuner app.
+//  And then follow https://github.com/Team364/BaseFalconSwerve#setting-constants
+//  This will tell you how to do the stuff marked as "wait for Josh"
+//  Such as setting the CANCoder offset, PID tuning, etc.
+//  Also verify that any unmarked constants such as driveInvert, angleInvert, and canCoderInvert are correct
+  //  by changing them one by one to true and seeing if that fixes anything.
+//  I *might* be available on Discord, but trying asking the FRC discord for technical questions https://discord.com/invite/frc
+// Even Teleop should not work until you fix at least the CANCoder offsets, if not more.
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.sensors.Pigeon2;
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -8,97 +8,103 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Swerve extends SubsystemBase {
-  public SwerveDriveOdometry swerveOdometry;
-  public SwerveModule[] mSwerveMods;
-  public Pigeon2 gyro;
-  private final Field2d m_field2d = new Field2d();
+	public SwerveDriveOdometry swerveOdometry;
+	public SwerveModule[] mSwerveMods;
+	// public Pigeon2 gyro;
+	public AHRS gyro = new AHRS(SerialPort.Port.kMXP);
+	private final Field2d m_field2d = new Field2d();
 
-  public Swerve() {
-    gyro = new Pigeon2(Constants.Swerve.pigeonID);
-    gyro.configFactoryDefault();
-    zeroGyro();
+	public Swerve() {
+		// gyro = new Pigeon2(Constants.Swerve.pigeonID);
+		// gyro.configFactoryDefault();
+		
+		gyro.calibrate();
+			
+		zeroGyro();
 
-    swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getYaw());
+		swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getYaw());
 
-    mSwerveMods =
-        new SwerveModule[] {
-          new SwerveModule(0, Constants.Swerve.Mod0.constants),
-          new SwerveModule(1, Constants.Swerve.Mod1.constants),
-          new SwerveModule(2, Constants.Swerve.Mod2.constants),
-          new SwerveModule(3, Constants.Swerve.Mod3.constants)
-        };
-  }
+		mSwerveMods =
+				new SwerveModule[] {
+					new SwerveModule(0, Constants.Swerve.Mod0.constants),
+					new SwerveModule(1, Constants.Swerve.Mod1.constants),
+					new SwerveModule(2, Constants.Swerve.Mod2.constants),
+					new SwerveModule(3, Constants.Swerve.Mod3.constants)
+				};
+	}
 
-  public void drive(
-      Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
-    SwerveModuleState[] swerveModuleStates =
-        Constants.Swerve.swerveKinematics.toSwerveModuleStates(
-            fieldRelative
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(
-                    translation.getX(), translation.getY(), rotation, getYaw())
-                : new ChassisSpeeds(translation.getX(), translation.getY(), rotation));
-    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
+	public void drive(
+			Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+		SwerveModuleState[] swerveModuleStates =
+				Constants.Swerve.swerveKinematics.toSwerveModuleStates(
+						fieldRelative
+								? ChassisSpeeds.fromFieldRelativeSpeeds(
+										translation.getX(), translation.getY(), rotation, getYaw())
+								: new ChassisSpeeds(translation.getX(), translation.getY(), rotation));
+		SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
 
-    for (SwerveModule mod : mSwerveMods) {
-      mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
-    }
-  }
+		for (SwerveModule mod : mSwerveMods) {
+			mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+		}
+	}
 
-  /* Used by SwerveControllerCommand in Auto */
-  public void setModuleStates(SwerveModuleState[] desiredStates) {
-    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.Swerve.maxSpeed);
+	/* Used by SwerveControllerCommand in Auto */
+	public void setModuleStates(SwerveModuleState[] desiredStates) {
+		SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.Swerve.maxSpeed);
 
-    for (SwerveModule mod : mSwerveMods) {
-      mod.setDesiredState(desiredStates[mod.moduleNumber], false);
-    }
-  }
+		for (SwerveModule mod : mSwerveMods) {
+			mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+		}
+	}
 
-  public Pose2d getPose() {
-    return swerveOdometry.getPoseMeters();
-  }
+	public Pose2d getPose() {
+		return swerveOdometry.getPoseMeters();
+	}
 
-  public void resetOdometry(Pose2d pose) {
-    swerveOdometry.resetPosition(pose, getYaw());
-  }
+	public void resetOdometry(Pose2d pose) {
+		swerveOdometry.resetPosition(pose, getYaw());
+	}
 
-  public SwerveModuleState[] getStates() {
-    SwerveModuleState[] states = new SwerveModuleState[4];
-    for (SwerveModule mod : mSwerveMods) {
-      states[mod.moduleNumber] = mod.getState();
-    }
-    return states;
-  }
+	public SwerveModuleState[] getStates() {
+		SwerveModuleState[] states = new SwerveModuleState[4];
+		for (SwerveModule mod : mSwerveMods) {
+			states[mod.moduleNumber] = mod.getState();
+		}
+		return states;
+	}
 
-  public void zeroGyro() {
-    gyro.setYaw(0);
-  }
+	public void zeroGyro() {
+		// gyro.setYaw(0);
+		gyro.reset();
+	}
 
-  public Rotation2d getYaw() {
-    return (Constants.Swerve.invertGyro)
-        ? Rotation2d.fromDegrees(360 - gyro.getYaw())
-        : Rotation2d.fromDegrees(gyro.getYaw());
-  }
+	public Rotation2d getYaw() {
+		return (Constants.Swerve.invertGyro)
+				? Rotation2d.fromDegrees(360 - gyro.getYaw())
+				: Rotation2d.fromDegrees(gyro.getYaw());
+	}
 
-  @Override
-  public void periodic() {
-    swerveOdometry.update(getYaw(), getStates());
-    m_field2d.setRobotPose(getPose());
+	@Override
+	public void periodic() {
+		swerveOdometry.update(getYaw(), getStates());
+		m_field2d.setRobotPose(getPose());
 
-    for (SwerveModule mod : mSwerveMods) {
-      SmartDashboard.putNumber(
-          "Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
-      SmartDashboard.putNumber(
-          "Mod " + mod.moduleNumber + " Integrated", mod.getState().angle.getDegrees());
-      SmartDashboard.putNumber(
-          "Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);
-      SmartDashboard.putData("Drive PID Controller", SwerveModule.mDrivePID);
-      SmartDashboard.putData("Angle PID Controller", SwerveModule.mAnglePID);
-    }
-  }
+		for (SwerveModule mod : mSwerveMods) {
+			SmartDashboard.putNumber(
+					"Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
+			SmartDashboard.putNumber(
+					"Mod " + mod.moduleNumber + " Integrated", mod.getState().angle.getDegrees());
+			SmartDashboard.putNumber(
+					"Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);
+			SmartDashboard.putData("Drive PID Controller", SwerveModule.mDrivePID);
+			SmartDashboard.putData("Angle PID Controller", SwerveModule.mAnglePID);
+		}
+	}
 }
